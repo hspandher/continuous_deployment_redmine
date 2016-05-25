e4866609a2444bbbd1c487a060404fd770e0e24b
hspandher <hspandher@outlook.com>
2016-05-25 17:44:53 +0530

    Change repo url

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :continuous_deployment do
   author 'HS Pandher'
   description 'A plugin to integrate redmine with Continuous Integration Server'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/hspandher/continuous_deployment_redmine'
   author_url 'https://github.com/hspandher/'
   require_dependency 'plugins/redmine_knowledgebase/lib/issue_patch'
 end
