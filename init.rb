38bdf2ba2f5e09d88a19367147b9dde3a3d74fb5
hspandher <hspandher@outlook.com>
2016-06-24 12:41:24 +0530

    Add admin template for plugin configuration

--- init.rb
+++ init.rb
@@ -8,12 +8,13 @@ Redmine::Plugin.register :continuous_deployment do
   require_dependency 'plugins/continuous_deployment/lib/issue_patch'
 
   settings default: {
-    target_project_names: ['candidate'],
-    candidate: {
-      deployment_server_url: "http://localhost:8001/redmine_response/",
-      default_request_params: {
-        auth_token: '343424324324'
+      target_project_names: ['candidate'],
+      candidate: {
+        deployment_server_url: "http://localhost:8001/redmine_response/",
+        default_request_params: {
+          auth_token: '343424324324'
+        }
       }
-    }
-  }
+    },
+    partial: 'settings/continuous_deployment_settings'
 end
