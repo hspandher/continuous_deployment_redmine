89659d29f705e65b4490cfb76896aee9e54f4bde
hspandher <hspandher@outlook.com>
2016-07-22 16:22:13 +0530

    Provide default settings as an example

--- init.rb
+++ init.rb
@@ -7,14 +7,15 @@ Redmine::Plugin.register :continuous_deployment do
   author_url 'https://github.com/hspandher/'
   require_dependency 'plugins/continuous_deployment/lib/issue_patch'
 
-  settings default: {
-      target_project_names: ['candidate'],
-      candidate: {
-        deployment_server_url: "http://hspandher:67ce59d90c9c3e3b47cd83d9b80684a0@localhost:8080/job/quality_assurance/buildWithParameters",
-        default_request_params: {
-          token: 'aHNwYW5kaGVy'
-        }
-      }
-    },
-    partial: 'settings/continuous_deployment_settings'
+  # Example Settings:- Create these in rails console for your projects
+  # settings default: {
+  #     target_project_names: ['candidate'],
+  #     candidate: {
+  #       deployment_server_url: "http://hspandher:67ce59d90c9c3e3b47cd83d9b80684a0@localhost:8080/job/quality_assurance/buildWithParameters",
+  #       default_request_params: {
+  #         token: 'aHNwYW5kaGVy'
+  #       }
+  #     }
+  #   },
+  #   partial: 'settings/continuous_deployment_settings'
 end
