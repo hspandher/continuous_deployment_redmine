15afb712da1d0fc8fc6139507f1d4c85c17688f2
hspandher <hspandher@outlook.com>
2016-07-28 13:16:58 +0530

    Add assignee in ci server params as well

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -33,11 +33,21 @@ module IssuePatch
       uri = URI(project_settings[:deployment_server_url])
 
       uri.query = URI.encode_www_form(
-        attributes.merge(extra_issue_attributes).merge(project_settings[:default_request_params])
+        attributes.merge(extra_issue_attributes).merge(assignee_params).merge(project_settings[:default_request_params])
       )
 
       Net::HTTP.post_form(uri, {})
     end
+  end
+
+  def assignee_params
+    original_assignee = assigned_to_was || assigned_to
+    {
+      initial_assignee_id: (original_assignee.id if original_assignee),
+      initial_assignee_email: (original_assignee.mail if original_assignee),
+      final_assignee_id: (assigned_to.id if assigned_to),
+      final_assignee_email: (assigned_to.mail if assigned_to)
+    }
   end
 
   def extra_issue_attributes
@@ -53,6 +63,6 @@ class Issue
 
   include IssuePatch
 
-  after_save :post_update
+  after_update :post_update
 
 end
